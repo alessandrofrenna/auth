dfd0611dab006653aba94a4973761816de54895c
Alessandro Frenna <alessandrofrenna95@gmail.com>
2017-10-07 17:49:13 +0200

    Module auth commit.

--- src/Auth.php
+++ src/Auth.php
@@ -6,5 +6,15 @@ use yii\base\Module;
 
 class Auth extends Module
 {
-    
+
+    public $controllerNamespace = 'frenna\auth\controllers';
+
+    public $defaultRoute = 'auth';
+
+    public $viewPath = 'Views/auth';
+
+    public function init()
+    {
+        parent::init();
+    }
 }
