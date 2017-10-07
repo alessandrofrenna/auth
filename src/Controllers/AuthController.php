b061b2f55a4af9c209de40cfdf36b26a62aa3e1a
Alessandro Frenna <alessandrofrenna95@gmail.com>
2017-10-07 18:46:36 +0200

    Allow inside behaviors inside auth controller

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
                 'rules' => [
                     [
                         'actions' => ['logout'],
-                        'allows'  => ['true'],
+                        'allow'   => ['true'],
                         'roles'   => ['@'],
                     ],                    
                 ],
